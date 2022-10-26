34cd1ebddbd2c4cb2e2d163559fa0eb2b265b784
Sergej <sergej.kubat18@gmail.com>
2022-10-26 08:07:37 +0200

    Hardhat configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
-require("@nomicfoundation/hardhat-toolbox");
+import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.17",
+
+const GANACHE_RPC_URL = process.env.GANACHE_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+export const defaultNetwork = "hardhat";
+
+export const networks = {
+    ganache: {
+        url: GANACHE_RPC_URL,
+        accounts: [PRIVATE_KEY],
+    },
 };
+
+export const gasReporter = {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+};
+
+export const solidity = "0.8.17";
